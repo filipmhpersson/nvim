2a9c55b93b77c471f54d1b8eb3836956932b2b6d
Filip Persson <filip.persson@ibinder.com>
2025-04-11 10:57:57 +0200

    CodeCompanion

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -1,34 +1,39 @@
 return {
-  --  {
-  --    'olimorris/codecompanion.nvim',
-  --    event = 'VeryLazy',
-  --    config = function()
-  --      require('codecompanion').setup {
-  --        adapters = {
-  --          copilot = function()
-  --            return require('codecompanion.adapters').extend('copilot', {
-  --              schema = {
-  --                model = {
-  --                  default = 'claude-3.7-sonneta',
-  --                },
-  --              },
-  --            })
-  --          end,
-  --        },
-  --        strategies = {
-  --          chat = {
-  --            adapter = 'copilot',
-  --          },
-  --          inline = {
-  --            adapter = 'copilot',
-  --          },
-  --        },
-  --      }
-  --    end,
-  --
-  --    dependencies = {
-  --      'nvim-lua/plenary.nvim',
-  --      'nvim-treesitter/nvim-treesitter',
-  --    },
-  --  },
+  {
+    'olimorris/codecompanion.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('codecompanion').setup {
+        adapters = {
+          copilot = function()
+            return require('codecompanion.adapters').extend('copilot', {
+              schema = {
+                model = {
+                  default = 'claude-3.7-sonnet',
+                },
+              },
+            })
+          end,
+        },
+        strategies = {
+          chat = {
+            adapter = 'copilot',
+          },
+          inline = {
+            adapter = 'copilot',
+          },
+          completion = {
+            adapter = 'copilot',
+            accept_keymaps = '<Tab>',
+            dismiss_keymaps = '<C-]>',
+          },
+        },
+      }
+    end,
+
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+  },
 }
